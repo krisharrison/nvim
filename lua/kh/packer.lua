0e7c898eb82b72054171e945b8a792ffb7c0defd
kh <kris.n.harrison@gmail.com>
2024-08-15 01:04:01 -0400

    LSP

--- lua/kh/packer.lua
+++ lua/kh/packer.lua
@@ -5,9 +5,48 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   
-  use {
+  use {--Telescope for search
 	  'nvim-telescope/telescope.nvim', tag = '0.1.8',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
+  use({--color theme
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function()
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}) --Tree sitter
+  use('nvim-treesitter/playground')
+  use('ThePrimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+  
+  use { 
+	  'VonHeikemen/lsp-zero.nvim',
+	  requires = {
+		  --LSP Support
+		  {'neovim/nvim-lspconfig'},
+		  {'williamboman/mason.nvim'},
+		  {'williamboman/mason-lspconfig.nvim'},
+
+
+		  --Autocompletion
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-buffer'},
+		  {'hrsh7th/cmp-path'},
+		  {'saadparwaiz1/cmp_luasnip'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'hrsh7th/cmp-nvim-lua'},
+
+
+		  --Snippets
+		  {'L3MON4D3/LuaSnip'},
+		  {'rafamadriz/friendly-snippets'},
+		}
+	}
+
+
+
 end)
