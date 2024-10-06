1a4f64a774483fcf65d580db81f627650262ac1f
kh <kris.n.harrison@gmail.com>
2024-10-06 19:14:42 -0400

    lualine

--- lua/kh/packer.lua
+++ lua/kh/packer.lua
@@ -44,7 +44,13 @@ return require('packer').startup(function(use)
 		  {'rafamadriz/friendly-snippets'},
 		}
 	}
-
-
+    use {
+            'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+    use {
+            'nvim-lualine/lualine.nvim',
+            requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 
 end)
